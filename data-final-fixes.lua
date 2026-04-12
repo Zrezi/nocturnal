5d9e5ce6f28df674961b55aed0bd5ee5676a9948
Zach Reznicek <zbrezi@gmail.com>
2026-04-12 00:34:07 -0500

    simplify other planet LUT

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -12,25 +12,15 @@ end
 
 if data.raw["planet"] then
     local constants = require("constants")
-    local nocturnal_prefix = constants.mod_path .. "/graphics/color_luts/"
-    
-    local night_luts = {
-        vulcanus = {"vulcanus-2-night.png"},
-        gleba = {"gleba-5-after-sunset.png", "gleba-6-before-dawn.png"},
-        fulgora = {"fulgora-3-after-sunset.png", "fulgora-4-before-dawn.png"}
-    }
+    local nocturnal_night_lut = constants.mod_path .. "/graphics/color_luts/lut-night.png"
     
     for _, planet_name in pairs({"vulcanus", "gleba", "fulgora", "aquilo"}) do
         local planet = data.raw["planet"][planet_name]
         if planet and planet.surface_render_parameters and planet.surface_render_parameters.day_night_cycle_color_lookup then
             for _, entry in pairs(planet.surface_render_parameters.day_night_cycle_color_lookup) do
                 local filename = entry[2]:match("([^/]+)$")
-                if night_luts[planet_name] then
-                    for _, night_filename in ipairs(night_luts[planet_name]) do
-                        if filename == night_filename then
-                            entry[2] = nocturnal_prefix .. filename
-                        end
-                    end
+                if filename:find("night.png") or filename:find("after%-sunset.png") or filename:find("before%-dawn.png") then
+                    entry[2] = nocturnal_night_lut
                 end
             end
         end
